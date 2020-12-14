a894c2c0c3dfa86276c4d6975ae9a6a7fdfe31af
Eric CARDENAS <cardenasmartineric@fpllefia.com>
2020-12-14 17:38:49 +0000

    a

--- index.js
+++ index.js
@@ -2,20 +2,25 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/m4/practica3_CardenasMartinEric/index.html'));
-});
+let localPath = '/m4/practica3_CardenasMartinEric/'
+
+app.use('/', express.static(path.join(__dirname + localPath)));
+
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname + localPath + 'index.html'));
+//     // res.sendFile(path.join(__dirname + '/m4/practica3_CardenasMartinEric/index.html'));
+// });
 
 app.get('/html/:html', function(req, res) {
-    res.sendFile(path.join(__dirname + '/m4/practica3_CardenasMartinEric/html/' + req.params.html));
+    res.sendFile(path.join(__dirname + localPath + 'html/' + req.params.html));
 });
 
 app.get('/css/:css', function(req, res) {
-    res.sendFile(path.join(__dirname + '/m4/practica3_CardenasMartinEric/css/' + req.params.css));
+    res.sendFile(path.join(__dirname + localPath + 'css/' + req.params.css));
 });
 
 app.get('/fotico/:img', function(req, res) {
-    res.sendFile(path.join(__dirname + '/m4/practica3_CardenasMartinEric/imagenes/' + req.params.img));
+    res.sendFile(path.join(__dirname + localPath + 'imagenes/' + req.params.img));
 });
 
 app.listen(8080);
